e8f8721c27cb1e725a70f1a2e6127dd127c02cf0
Utku Sürgit <usurgit@gmail.com>
2024-02-24 15:55:00 +0300

    test

---
+++ src/upsideDownTriangle/Main.java
@@ -0,0 +1,22 @@
+package upsideDownTriangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+
+        for (int i = n ; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
